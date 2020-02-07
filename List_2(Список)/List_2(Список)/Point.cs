38647e5c64bc55a11036d8179b97b89bc374bd2f
Vasily Kulikov <zel.slonik17@gmail.com>
2020-02-08 00:19:34 +0300

    Делаем домашнее задание "Список 2"

---
+++ List_2(Список)/List_2(Список)/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List_2_Список_
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public Point()
+        {
+
+        }
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
